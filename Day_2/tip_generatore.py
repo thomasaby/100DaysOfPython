0659d41073dc1413268d5a3905de2c96de2b9259
thomasaby <65249288+thomasaby@users.noreply.github.com>
2025-04-24 19:36:59 -0700

    update Day2

--- Day_2/tip_generatore.py
+++ Day_2/tip_generatore.py
@@ -1,6 +1,6 @@
 
 print("Welcome to the tip calculator!\n\n")
-total = float(input("Enter total amount\n"))
+total = float(input("Enter total amount\n $"))
 tip_percent = float(input("Enter tip percentage\n"))
 final_amount = total + (total*(tip_percent/100))
 no_payers = int(input("How many people to split the bill with? \n"))
